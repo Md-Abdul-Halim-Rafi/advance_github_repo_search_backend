c62ecb2a50ac3c38079c63c0353c7e4dfbb5808b
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2022-06-24 20:55:03 +0600

    rate limiter added to api route

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,19 +1,26 @@
+const { Router } = require("express");
 const { errors, celebrate } = require("celebrate");
 
 const { searchedRepositories } = require("../validations/search");
 const { getSearchedRepositories } = require("../controllers/serach");
 
-const { Router } = require("express");
+const rateLimitMiddleware = require("../middlewares/rate-limiter");
+const createRateLimiter = require("../../utils/rate-limiter");
 
 const router = Router();
 
 const searchRoutes = (app) => {
 
-   router.get("/repositories", celebrate(searchedRepositories), getSearchedRepositories);
+	router.get(
+		"/repositories",
+		rateLimitMiddleware(createRateLimiter("search-limit", 50, 600)),
+		celebrate(searchedRepositories), 
+		getSearchedRepositories
+	);
 
-   router.use(errors());
+	router.use(errors());
 
-   app.use("/special-search", router);
+	app.use("/special-search", router);
 }
 
 module.exports = searchRoutes;
---
+++ utils/redis-conf.js
@@ -0,0 +1,50 @@
+const redisConf = {
+    development: {
+        host: "host.docker.internal",
+        port: "6379",
+        family: "4",
+        password: "123456",
+        db: 0,
+        showFriendlyErrorStack: true,
+        retryStrategy: function() {
+            const delay = 20000;
+            return delay;
+        },
+    },
+    production: {
+        host: "127.0.0.1",
+        port: "6379",
+        family: "4",
+        password: "%%NUPORT!!",
+        db: 0,
+        showFriendlyErrorStack: true,
+        retryStrategy: function() {
+            const delay = 20000;
+            return delay;
+        },
+    }
+}
+
+const redisConfRateLimiter = {
+    development: {
+        host: "host.docker.internal",
+        port: "6379",
+        family: "4",
+        password: "123456",
+        db: 0,
+        enableOfflineQueue: false
+    },
+    production: {
+        host: "127.0.0.1",
+        port: "6379",
+        family: "4",
+        password: "%%NUPORT!!",
+        db: 0,
+        enableOfflineQueue: false
+    }
+}
+
+module.exports = {
+    redisConf,
+    redisConfRateLimiter
+}
